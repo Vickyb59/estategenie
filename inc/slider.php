05eaf208bfeb9b1bd9381a8bf6e737c1d82624da
Victor Bateren <baterenvictor@gmail.com>
2021-08-23 14:05:24 +0100

    Update slider.php

--- inc/slider.php
+++ inc/slider.php
@@ -6,8 +6,8 @@
                             <div class="silder-content" data-swiper-parallax="-40%">
                                 <div class="inner-content">
                                     <h6 class="sub-title">We Bring Management and Residents Closer</h6>
-                                    <h1 class="title">Welcome to <span class="text-primary">Estate Genie</span></h1>
-                                    <p class="m-b30">Simplify the management duties and improve communication within your community by using our mobile solution. Make your life easier with Estate Genie. &nbsp;Our focus is on creating a one-stop ecosystem for residential and commercial communities. </p>
+                                    <h1 class="title">Welcome to <span class="text-primary">Orion Limited</span></h1>
+                                    <p class="m-b30">Orion LTD, is a dynamic organization focused on crafting unique and innovative digital solutions to daily problems. &nbsp;Our primary focus is on the previously unexplored residential industry. &nbsp;It is our ambition to create the first and only network of online residential neighborhoods. </p>
                                     <a href="onboarding" class="btn shadow-primary btn-primary">Get Started</a>
                                 </div>
                             </div>
