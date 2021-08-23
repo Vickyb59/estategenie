e2a66e58b4bcb6e2f39224466188e602d789a88a
Victor Bateren <baterenvictor@gmail.com>
2021-08-23 14:13:05 +0100

    Update slider.php

--- inc/slider.php
+++ inc/slider.php
@@ -5,10 +5,9 @@
                         <div class="dz-slide-item" style="background-image:url(images/main-slider/slider-bg1.png);">
                             <div class="silder-content" data-swiper-parallax="-40%">
                                 <div class="inner-content">
-                                    <h6 class="sub-title">We Bring Management and Residents Closer</h6>
-                                    <h1 class="title">Welcome to <span class="text-primary">Orion Limited</span></h1>
+                                    <h1 class="title">Welcome to <span class="text-primary">Orion Ltd</span></h1>
                                     <p class="m-b30">Orion LTD, is a dynamic organization focused on crafting unique and innovative digital solutions to daily problems. &nbsp;Our primary focus is on the previously unexplored residential industry. &nbsp;It is our ambition to create the first and only network of online residential neighborhoods. </p>
-                                    <a href="onboarding" class="btn shadow-primary btn-primary">Get Started</a>
+                                    <a href="about" class="btn shadow-primary btn-primary">Read More</a>
                                 </div>
                             </div>
                             <div class="slider-img" data-swiper-parallax-y="-40%" data-swiper-parallax-x="20%" data-swiper-parallax-opacity="0">
@@ -35,9 +34,8 @@
                         <div class="dz-slide-item" style="background-image:url(images/main-slider/slider-bg1.png);">
                             <div class="silder-content" data-swiper-parallax="-40%">
                                 <div class="inner-content">
-                                    <h6 class="sub-title">We Bring Management and Residents Closer</h6>
-                                    <h1 class="title">Welcome to <span class="text-primary">Estate Genie</span></h1>
-                                    <p class="m-b30">Simplify the management duties and improve communication within your community by using our mobile solution. Make your life easier with Estate Genie. &nbsp;Our focus is on creating a one-stop ecosystem for residential and commercial communities. </p>
+                                    <h1 class="title">Executive Summary</span></h1>
+                                    <p class="m-b30">The Estate Genie is an Estate Management Mobile Application Solution specifically designed to provide Goods, Services and Previledges to: Residential Estate Management Institutions, Individual Residents and Service Providers in any given residential estate/community at below pump price.</p>
                                     <a href="onboarding" class="btn shadow-primary btn-primary">Get Started</a>
                                 </div>
                             </div>
